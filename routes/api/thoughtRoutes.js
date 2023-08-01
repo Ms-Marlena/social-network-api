9beda572dc877dc81a3e95714b48ba1c01d2cea4
Marlena Hooker Moore <132782689+Ms-Marlena@users.noreply.github.com>
2023-08-01 12:45:13 -0400

    added thought routes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,23 @@
+const router = require("express").Router();
+const {
+  getThoughts,
+  getSingleThought,
+  createThought,
+  deleteThought,
+  addReaction,
+  removeReaction,
+} = require("../../controllers/studentController");
+
+// /api/thoughts
+router.route("/").get(getThoughts).post(createThought);
+
+// /api/thoughts/:thoughtId
+router.route("/:thoughtId").get(getSingleThought).delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions
+router.route("/:thoughtId/reactions").post(addReaction);
+
+// /api/thoughts/:thoughtId/reactions/:assignmentId
+router.route("/:thoughtId/reactions/:assignmentId").delete(removeReaction);
+
+module.exports = router;
